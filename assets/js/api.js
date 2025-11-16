41f3d0ceda6f1dec67d0a0fb973248d9aec5fcf8
sequeiracrea <sequeiracrea@gmail.com>
2025-11-16 16:19:04 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,14 @@
+// assets/js/api.js
+
+const API_URL = "https://indoor-sim-server.onrender.com/data";
+
+export async function fetchLiveData() {
+    try {
+        const res = await fetch(API_URL);
+        return await res.json();
+    } catch (err) {
+        console.error("Erreur API:", err);
+        return null;
+    }
+}
 
