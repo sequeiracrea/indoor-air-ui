df61863d41fc360d7e3656e460d2eb1f7f9e1a10
sequeiracrea <sequeiracrea@gmail.com>
2025-11-16 16:22:25 +0100

    Update charts.js

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,1 +1,25 @@
+// assets/js/charts.js
+export function scatterChart(ctx, points) {
+    return new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: "Relation",
+                data: points
+            }]
+        }
+    });
+}
+
+export function lineChart(ctx, values, labels) {
+    return new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels,
+            datasets: [{
+                data: values
+            }]
+        }
+    });
+}
 
